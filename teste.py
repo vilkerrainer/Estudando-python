d0f69c8a98053d1f23bafa61d4551d9c1b5c7858
vilkerrainer <vilkerrainer1@gmail.com>
2024-06-19 10:02:46 -0300

    Modificando a pasta teste para um'comparador' de digitos, com mostrador de tentativas e tempo de run code

--- teste.py
+++ teste.py
@@ -1,7 +1,32 @@
-dict = {
-    "nome": "Vilker",
-    "idade": 18,
-    "altura": 174,
-}
+import string
+import random
+import time
 
-print(dict["nome"])
+contador = 1
+inicio = time.time()
+medida = ""
+
+def gerar_senha(tamanho):
+    caracteres = string.ascii_letters + string.digits
+    senha = ''.join(random.choice(caracteres) for i in range(tamanho))
+    return senha
+
+while True:
+    fim = time.time()
+    senha = gerar_senha(4)
+    contador += 1
+    
+    
+    if fim - inicio < 1:
+        medida = "ms"
+    else:
+        medida = "s"
+    
+    print(f"{contador} tentativas")
+    
+    if senha == "jC7t":
+        print(f"Você achou a o {senha} em {contador} tentativas")
+        print(f"\nO Tempo gasto foi de {fim - inicio:.2f}{medida}")
+        break
+    else:
+        continue
